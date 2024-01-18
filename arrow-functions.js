41f82a28408212fb82685f970341ee1fbd542da4
Arukwe Obo-Eba <oboebafriday@gmail.com>
2024-01-18 04:09:19 +0000

    Arrow functions review

--- arrow-functions.js
+++ arrow-functions.js
@@ -3,12 +3,38 @@
  * command node arrow-functions.js in the terminal
  */
 
+
 // Vanilla JavaScript Function
 
+// function addTwoNumbers(a, b) {
+// // code block
+// return a + b; 
+// }
+
+// let sum = addTwoNumbers(3, 5);
+// console.log(sum);
+
 // Arrow Function With Parameters
-
+const addTwoNumbers = (a, b) => {
+    // code block
+    return a + b; 
+    }
+    
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 // Single Line Arrow Function With Parameters
-
+//const addTwoNumber = (a, b) => (a + b);  valid
+const addTwoNumber = (a, b) => a + b;
+let sum2 = addTwoNumbers(3, 6);
+console.log(sum2);
 // Implicit Returns
-
+const saySomething = message => console.log(message);
+saySomething("Hello world!");
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiline string!
+    </p>
+    `
+)
+console.log(returnMultipleLines());
